2cc100a7e6bfceb89530372a45d3de136baa0aa1
s3lven <erizjsub2@gmail.com>
2024-06-03 14:56:04 -0700

    implemented card design. needs multiline truncate

--- src/app/components/EventComponent.tsx
+++ src/app/components/EventComponent.tsx
@@ -1,38 +1,48 @@
 import Image from "next/image"
 import Link from "next/link"
 
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { Badge } from "@/components/ui/badge"
 
 const EventComponent = () => {
     return (
-        <Link href={'#'}>
-            <div className="flex flex-col max-w-[410px] h-[512px] relative border-[1.5px] border-black">
-                <div className="relative h-[200px]">
+        <Card className="max-h-[600px] flex flex-col overflow-hidden">
+            <CardHeader className="relative">
+                <div className="relative w-full h-[250px]">
                     <Image 
                         src="/images/kendo-background.jpg"
                         alt="background image of two kendoka hitting men"
                         sizes="100vw"
                         quality={100}
                         fill
-                        style={{
-                            objectFit: 'cover',
-                        }}
+                        className="object-cover rounded-lg"
                     />
                 </div>
-                <div className="bg-white absolute top-2 right-2 text-center p-2 drop-shadow-sm aspect-square">
+                <div className="bg-white absolute top-10 right-10 text-center p-2 drop-shadow-sm aspect-square">
                     <p className="text-xs">Fri</p>
                     <h1 className="font-bold text-2xl">09</h1>
                     <p className="text-xs">Feb 2024</p>
                 </div>
-                <div className="flex flex-col flex-1 justify-start items-start px-2 py-5 gap-2 ">
-                    <p className="bg-gray-300 text-xs p-1">Category</p>
-                    <h1 className="text-2xl font-bold">Exciting Event</h1>
-                    <p className="text-xs">Location</p>
-                    <p className="text-sm text-pretty">Join us at this upcoming Federation event for a thrilling kendo event.</p>
-                </div>
-                <button className="text-black text-xs w-fit self-end px-2 py-3">See Details {">"}</button>
-            </div>
-        </Link>
-    ) 
+            </CardHeader>
+            <CardContent className="flex flex-col gap-2.5 overflow-hidden">
+                <Badge variant="secondary" className="self-start text-base">Category</Badge>
+                <h1 className="font-bold text-2xl">Event Title</h1>
+                <p className="font-bold text-base">Location</p>
+                <p className="text-ellipsis">Join us at this upcoming Federation event for a thrilling kendo event event event event event event event event event event event event event event event event event event event event event event event event event event event event event event event event event event event event event event event event event event </p>
+            </CardContent>
+            <CardFooter className="flex justify-end">
+                <Button variant={"outline"} asChild>
+                    <Link href="#" className="text-right">More Details</Link>
+                </Button>
+            </CardFooter>
+        </Card>
+    )   
 }
 
 export default EventComponent
