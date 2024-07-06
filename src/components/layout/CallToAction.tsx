dbb43e322d8601c6dfd0f4c8efcb72209d96d4a6
s3lven <erizjsub2@gmail.com>
2024-07-05 18:44:46 -0700

    feat<home>: finish home page, ensuring alignment of all sections

--- src/components/layout/CallToAction.tsx
+++ src/components/layout/CallToAction.tsx
@@ -4,8 +4,7 @@ import Link from "next/link"
 
 const CallToAction = () => {
     return (
-        <div className="relative overflow-hidden flex flex-col justify-center items-center px-8 py-6 
-                        lg:px-[100px] lg:py-[100px]">
+        <div className="relative overflow-hidden flex flex-col justify-center items-center">
             <Image 
                 src="/images/kendo-background.jpg"
                 alt="background image of two kendoka hitting men"
@@ -14,14 +13,13 @@ const CallToAction = () => {
                 fill
                 className="object-cover"
             />
-            <div className="z-0 flex flex-col items-center gap-2
-                            lg:flex-row lg:justify-between">
+            <div className="z-0 flex flex-col justify-between items-center gap-2 max-w-[1440px]
+                            lg:flex-row lg:justify-between px-8 py-6 lg:px-[100px] lg:py-10">
                 <h1 className="font-bold text-2xl text-white text-center
-                                lg:text-5xl lg:text-left text-balance lg:leading-relaxed">
+                                lg:text-5xl lg:text-left lg: text-balance lg:leading-relaxed">
                                     Join now and become a valued member of the NCKF community!
                 </h1>
-                <Button asChild className="bg-white text-black hover:bg-white/80 font-semibold
-                                            lg:text-2xl px-6 py-8">
+                <Button asChild size="lg" className="bg-white text-black hover:bg-white/80 font-semibold lg:text-lg">
                     <Link href="#">Join NCKF</Link>
                 </Button>
             </div>
